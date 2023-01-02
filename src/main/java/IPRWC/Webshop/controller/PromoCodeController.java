ef9febbfeaa33ba8e5a1e72734a7b942d16cbf6b
Cors Schonenberg <s1135206@student.hsleiden.nl>
2023-01-02 16:59:47 +0100

    Feat: added promocode controller

--- src/main/java/IPRWC/Webshop/controller/PromoCodeController.java
+++ src/main/java/IPRWC/Webshop/controller/PromoCodeController.java
@@ -1,11 +1,12 @@
 package IPRWC.Webshop.controller;
 
-import IPRWC.Webshop.dao.OrderDao;
 import IPRWC.Webshop.dao.ProductDao;
 import IPRWC.Webshop.dao.ProductRepository;
+import IPRWC.Webshop.dao.PromoCodeDao;
 import IPRWC.Webshop.model.ApiResponse;
 import IPRWC.Webshop.model.Order;
 import IPRWC.Webshop.model.Product;
+import IPRWC.Webshop.model.PromoCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -13,41 +14,40 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @Controller
-@RequestMapping(value = "/api/v1/product")
+@RequestMapping(value = "/api/v1/promocode")
 @CrossOrigin("*")
-public class ProductController {
-    private final ProductDao productDao;
+public class PromoCodeController {
+    private final PromoCodeDao promoCodeDao;
 
-    public ProductController(ProductDao productDao,
-                             ProductRepository productRepository) {
-        this.productDao = productDao;
+    public PromoCodeController(PromoCodeDao promoCodeDao) {
+        this.promoCodeDao = promoCodeDao;
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     @ResponseBody
-    public ApiResponse<ArrayList<Order>> products() {
-        ArrayList<Product> products = this.productDao.getAllProducts();
-        return new ApiResponse(HttpStatus.ACCEPTED, products);
+    public ApiResponse<ArrayList<Order>> promoCodes() {
+        ArrayList<PromoCode> promoCodes = this.promoCodeDao.getAllCodes();
+        return new ApiResponse(HttpStatus.ACCEPTED, promoCodes);
     }
 
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
     @ResponseBody
-    public ApiResponse postProducts(@RequestBody Product product) {
-        this.productDao.saveToDatabase(product);
+    public ApiResponse postPromoCodes(@RequestBody PromoCode promoCode) {
+        this.promoCodeDao.saveToDatabase(promoCode);
         return new ApiResponse(HttpStatus.ACCEPTED, "You posted some data!");
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
     @ResponseBody
-    public ApiResponse deleteProduct(@PathVariable Integer id) {
-        this.productDao.deleteProductFromDatabase(id);
+    public ApiResponse deletePromoCode(@PathVariable Integer id) {
+        this.promoCodeDao.deleteCodeFromDatabase(id);
         return new ApiResponse(HttpStatus.ACCEPTED, "You deleted some data!");
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.PUT)
     @ResponseBody
-    public ApiResponse updateProduct(@RequestBody Product product) {
-        this.productDao.saveToDatabase(product);
+    public ApiResponse updatePromoCode(@RequestBody PromoCode promoCode) {
+        this.promoCodeDao.saveToDatabase(promoCode);
         return new ApiResponse(HttpStatus.ACCEPTED, "You updated some data!");
     }
 }
