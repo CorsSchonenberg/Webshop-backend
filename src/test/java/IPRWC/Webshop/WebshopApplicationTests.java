5bcf15b898ac71d1a2375d74a44ad3065ce4b0db
Cors Schonenberg <s1135206@student.hsleiden.nl>
2023-01-02 15:36:34 +0100

    init: Add existing project files to Git

---
+++ src/test/java/IPRWC/Webshop/WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package IPRWC.Webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
