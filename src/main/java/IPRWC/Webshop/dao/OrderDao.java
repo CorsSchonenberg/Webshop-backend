510edd8fb55f364429e1286553cedbef555df27b
Cors Schonenberg <s1135206@student.hsleiden.nl>
2023-01-02 16:35:02 +0100

    Feat: added Order dao

---
+++ src/main/java/IPRWC/Webshop/dao/OrderDao.java
@@ -0,0 +1,24 @@
+package IPRWC.Webshop.dao;
+
+import IPRWC.Webshop.model.Order;
+import IPRWC.Webshop.model.PromoCode;
+
+import java.util.ArrayList;
+
+public class OrderDao {
+
+    private final OrderRepository orderRepository;
+
+    public OrderDao(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+    public void saveToDatabase(Order order) {
+        this.orderRepository.save(order);
+    }
+    public ArrayList<Order> getAllOrders() {
+        return (ArrayList<Order>) this.orderRepository.findAll();
+    }
+    public void deleteOrderFromDatabase(Integer id) {
+        this.orderRepository.deleteById(id);
+    }
+}
